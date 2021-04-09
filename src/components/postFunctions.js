8a44fe943afd2f7f22f1bc027174ff754aff9a72
Morad17 <morad123@hotmail.co.uk>
2021-04-09 21:43:15 +0100

    update

---
+++ src/components/postFunctions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/scss/base.scss'
+
+const postFunctions = (props) => {
+
+    return(
+        <div>
+            <Card style={{ width:'33%'}}><button>New Post</button></Card>
+        </div>
+    )
+}
+
+    export default PostFunctions
