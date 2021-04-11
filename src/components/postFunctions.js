1ccb5292b0a3e814a20f1b7eb46757242d1b4eb3
Morad17 <morad123@hotmail.co.uk>
2021-04-11 17:05:19 +0100

    creating posts

--- src/components/postFunctions.js
+++ src/components/postFunctions.js
@@ -8,14 +8,19 @@ const PostFunctions = (props) => {
 
     return(
         <div className="post-functions">
-            <Card style={{ width:'33%', display:'flex', justifyContent:'space-between'}}>
-                <div className="post-div">
+            <Card style={{ width:'97%', display:'flex', justifyContent:'space-between'}}>
+                <div className="">
+                    
+                </div>
+            </Card>
+
+
+            {/* <div className="post-div">
                     <img className="new-post-thumb"src="./images/new-post.png" alt=""/><button>New Post</button>
                 </div>
                 <div className="post-div">
                     <img className="edit-post-thumb"src="./images/edit-post.png" alt=""/><button>Edit Post</button>
-                </div>
-            </Card>
+                </div> */}
         </div>
     )
 }
