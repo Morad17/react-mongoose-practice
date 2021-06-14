5df5c51ec7ed7be8ab4ca703e0fe6349c3b737b1
Morad17 <morad123@hotmail.co.uk>
2021-06-14 23:01:20 +0100

    updates

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -10,6 +10,7 @@ import '../assets/scss/_blog-card.scss'
 **/
 
 const BlogCard = (props) => {
+  
   return(
     <div prop={props} className="BlogCard">
         <div className="BlogCard-image">
@@ -18,9 +19,9 @@ const BlogCard = (props) => {
         <h2 className="BlogCard-title">{props.blog.blogTitle}</h2>
         <div className="BlogCard-text">
           <p>{props.blog.language}</p>
-          <p>{props.blog.username},{props.date}</p>
+          <p>{props.blog.username},{props.date} {console.log(props.date)}</p>
           <div className="col">
-            <Link to={"/edit/"+props.blog._id}>edit</Link>| <a href="#" onClick={() => { props.deleteBlog(props.blog._id) }}>delete</a>
+            <Link to={"/edit/"+props.blog._id}><a class="edit-button"href="#">Edit</a></Link> <a class="delete-button"href="#" onClick={() => { props.deleteBlog(props.blog._id) }}>Delete</a>
           </div>
         </div>
     </div>
